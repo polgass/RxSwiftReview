78d5f4bad9e26b3a5cd8a5fc4cb7e72370801815
Paul Sevilla <psevilla@macquarietelecom.com>
2017-06-06 16:02:51 +1000

    Complete chapter 2

---
+++ RxSwiftPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var array = [1, 2, 3]
+for number in array {
+  print(number)
+  array = [4, 5, 6]
+}
+print(array)
